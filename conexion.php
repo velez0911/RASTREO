4b877d89842aa95dcad9545516d95764798b04ba
velez0911 <a.24308051280911@cbtis128.edu.mx>
2026-09-23 22:47:54 -0600

    Fix PHP opening tag and update headers
    Removed duplicate PHP opening tag and adjusted headers.

--- conexion.php
+++ conexion.php
@@ -9,8 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-?>
-<?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
